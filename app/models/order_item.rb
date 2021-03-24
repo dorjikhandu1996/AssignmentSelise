a7f515dc0ead32762b5d3cdb420316e4bb96c002
dorji khandu <khandudorji47@gmail.com>
2021-03-24 23:02:01 +0545

    add-to-cart-action

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,32 @@
 class OrderItem < ApplicationRecord
     belongs_to :order
     belongs_to :product
+    
+    before_save :set_unit_cost
+    before_save :set_total
+
+    def unit_price
+        if persisted?
+            self[:unit_cost]
+        else
+            product.price 
+         end
+    end
+
+    def total
+        product.price * quantity
+    end
+
+    private
+    
+    def set_unit_price
+        self[:unit_cost] = unit_price
+    end
+
+    def set_total
+        self[:total] = total * quantity
+    end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'carts/show'
   resources :discounts
   resources :products
   resources :shops, only:[:index,:show]
   root to:'shops#index'
+  resources :order_items
+  resource :carts, only:[:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
